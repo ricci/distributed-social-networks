ce505cdab6eaa42888e2132f171f4fd73ed913a3
Robert Ricci <ricci@cs.utah.edu>
2025-08-29 00:19:50 -0600

    Add more information for the services

--- www/data.js
+++ www/data.js
@@ -1,5 +1,19 @@
 var data = {
   "lastUpdate": "08-27-2025",
-  "fedi": { "HHI": 581 },
-  "at": { "HHI": 9912 }
+  "fedi": {
+     "HHI": 581,
+     "servers": 23179,
+     "biggest_abs": 2858785,
+     "biggest_pct": 19.84,
+     "rest_abs": 11550180,
+     "rest_pct": 80.16,
+  },
+  "at"  : {
+     "HHI": 9912,
+     "servers":  1217,
+     "biggest_abs": 12167895,
+     "biggest_pct": 99.56,
+     "rest_abs": 54127,
+     "rest_pct": 0.44
+  }
 }
