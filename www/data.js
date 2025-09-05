f6c0addea481ad0ba1d572dfddeba4b9caae3025
Robert Ricci <ricci@cs.utah.edu>
2025-09-04 23:02:01 -0600

    Update data

--- www/data.js
+++ www/data.js
@@ -1,23 +1,5 @@
 var data = {
-  "lastUpdate": "08-30-2025",
-  "fedi": {
-     "HHI": 727,
-     "shannon": 4.9015,
-     "simpson": 0.9273,
-     "servers": 18592,
-     "biggest_abs": 277461,
-     "biggest_pct": 24.87,
-     "rest_abs": 838102,
-     "rest_pct": 75.13,
-  },
-  "at"  : {
-     "HHI": 9911,
-     "shannon": 0.0379,
-     "simpson": 0.0089,
-     "servers":  1643,
-     "biggest_abs": 12261600,
-     "biggest_pct": 99.55,
-     "rest_abs": 55128,
-     "rest_pct": 0.45
-  }
+  "lastUpdate": "09-04-2025",
+  "fedi": {"HHI": 726, "shannon": 4.89, "simpson": 0.9274, "servers": 18546, "biggest_abs": 276100, "biggest_pct": 24.55, "rest_abs": 848618, "rest_pct": 75.45, "b_vals": [[25, 2], [50, 14], [75, 100], [90, 505], [99, 7899]]}, 
+  "at"  : {"HHI": 9911, "shannon": 0.0374, "simpson": 0.0088, "servers": 1552, "biggest_abs": 12448268, "biggest_pct": 99.56, "rest_abs": 55426, "rest_pct": 0.44, "b_vals": [[25, 1], [50, 1], [75, 1], [90, 1], [99, 1]]}
 }
