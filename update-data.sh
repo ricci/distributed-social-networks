d53b38918bbd1a08f15d6d427a3dc3fb82ad30bd
Robert Ricci <ricci@cs.utah.edu>
2025-09-10 21:05:44 -0600

    Fix filename

--- update-data.sh
+++ update-data.sh
@@ -3,10 +3,10 @@
 python3 ./fetch-bsky.py &&
     python3 ./fetch-fedilist.py &&
     python3 ./fetch-sh.py &&
-    jq --argjson val "$(python3 hhi.py --json fedilist-fromhtml.csv)" '.fedi = $val' www/data.js > data.js.tmp && mv data.js.tmp www/data.js &&
-    jq --argjson val "$(python3 hhi.py --json atproto-bsky-relay.csv)" '.at = $val' www/data.js > data.js.tmp && mv data.js.tmp www/data.js &&
-    jq --argjson val "$(python3 hhi.py --json sh-fromhtml.csv)" '.git = $val' www/data.js > data.js.tmp && mv data.js.tmp www/data.js &&
-    jq --arg val "$(date +'%m-%d-%y')" '.lastUpdate = $val' www/data.js > data.js.tmp && mv data.js.tmp www/data.js &&
+    jq --argjson val "$(python3 hhi.py --json fedilist-fromhtml.csv)" '.fedi = $val' www/data.json > data.json.tmp && mv data.json.tmp www/data.json &&
+    jq --argjson val "$(python3 hhi.py --json atproto-bsky-relay.csv)" '.at = $val' www/data.json > data.json.tmp && mv data.json.tmp www/data.json &&
+    jq --argjson val "$(python3 hhi.py --json sh-fromhtml.csv)" '.git = $val' www/data.json > data.json.tmp && mv data.json.tmp www/data.json &&
+    jq --arg val "$(date +'%m-%d-%y')" '.lastUpdate = $val' www/data.json > data.json.tmp && mv data.json.tmp www/data.json &&
     echo "var data =  $(cat www/data.json)" > www/data.js
 
     
