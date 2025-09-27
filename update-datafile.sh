68d177389d7d457f4878f391dd15291db96803e7
Robert Ricci <ricci@cs.utah.edu>
2025-09-27 11:47:26 -0600

    update the date too

--- update-datafile.sh
+++ update-datafile.sh
@@ -20,5 +20,7 @@ GITFILE_OLD="data/git/$(ls -1 data/git | ./weekago.sh)"
 
 /usr/local/bin/jq --argjson val "$(sh ./diff-from-current.sh fedi $GITFILE $GITFILE_OLD)" '.trends.git.weekly_shannon = $val' www/data.json > data.json.tmp && mv data.json.tmp www/data.json
 
+/usr/local/bin/jq --arg val "$(date +"%m-%d-%Y")" '.lastUpdate = $val' < www/data.json > data.json.tmp && mv data.json.tmp www/data.json
+
 echo "var data =  $(cat www/data.json)" > www/data.js
 
