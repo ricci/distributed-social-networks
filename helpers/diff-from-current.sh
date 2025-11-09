775b630e5e6306ccfe3b8c1ce8c817225a8b2531
Robert Ricci <ricci@cs.utah.edu>
2025-11-09 16:10:53 -0700

    Fix some paths

--- helpers/diff-from-current.sh
+++ helpers/diff-from-current.sh
@@ -4,8 +4,8 @@ WHICH=$1
 CSV=$2
 CSV2=$3
 
-CUR=$(python3 ./hhi.py --json $CSV | jq .shannon)
-PREV=$(python3 ./hhi.py --json $CSV2 | jq .shannon)
+CUR=$(python3 ./centralization-stats.py --json $CSV | jq .shannon)
+PREV=$(python3 ./centralization-stats.py --json $CSV2 | jq .shannon)
 
 DIFF=$(echo "$CUR - $PREV" | bc -l)
 
--- helpers/update-datafile.sh
+++ helpers/update-datafile.sh
@@ -3,9 +3,9 @@
 ATFILE="data/at/$(./helpers/newest.sh data/at)"
 FEDIFILE="data/fedi/$(./helpers/newest.sh data/fedi)"
 GITFILE="data/git/$(./helpers/newest.sh data/git)"
-HOSTFILE="worldwide.csv"
-DNSFILE="dns-byid.csv"
-CERTFILE="cert-byid.csv"
+HOSTFILE="data-static/worldwide.csv"
+DNSFILE="data-static/dns-byid.csv"
+CERTFILE="data-static/cert-byid.csv"
 
 ATFILE_OLD="data/at/$(ls -1 data/at |  ./helpers/weekago.sh)"
 FEDIFILE_OLD="data/fedi/$(ls -1 data/fedi | ./helpers/weekago.sh)"
@@ -25,7 +25,7 @@ GITFILE_OLD="data/git/$(ls -1 data/git | ./helpers/weekago.sh)"
 
 /usr/local/bin/jq --argjson val "$(sh ./helpers/diff-from-current.sh fedi $FEDIFILE $FEDIFILE_OLD)" '.trends.fedi.weekly_shannon = $val' www/data.json > data.json.tmp && mv data.json.tmp www/data.json
 
-/usr/local/bin/jq --argjson val "$(sh ./helphers/diff-from-current.sh at $ATFILE $ATFILE_OLD)" '.trends.at.weekly_shannon = $val' www/data.json > data.json.tmp && mv data.json.tmp www/data.json
+/usr/local/bin/jq --argjson val "$(sh ./helpers/diff-from-current.sh at $ATFILE $ATFILE_OLD)" '.trends.at.weekly_shannon = $val' www/data.json > data.json.tmp && mv data.json.tmp www/data.json
 
 /usr/local/bin/jq --argjson val "$(sh ./helpers/diff-from-current.sh fedi $GITFILE $GITFILE_OLD)" '.trends.git.weekly_shannon = $val' www/data.json > data.json.tmp && mv data.json.tmp www/data.json
 
