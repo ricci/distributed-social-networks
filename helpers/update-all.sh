8db4844067a39f781e346ecf68161446c77207fc
Robert Ricci <ricci@cs.utah.edu>
2025-11-09 15:57:06 -0700

    Update to current paths

--- helpers/update-all.sh
+++ helpers/update-all.sh
@@ -5,10 +5,10 @@ TIMESTAMP=`date +"%Y-%m-%dT%H:%M:%S"`
 echo "Running at $TIMESTAMP"
 
 echo "Fetching BlueSky data"
-python3 ./fetch-bsky.py data/at/${TIMESTAMP}__atproto-bsky-relay.csv
+python3 ./data-fetchers/fetch-bsky.py data/at/${TIMESTAMP}__atproto-bsky-relay.csv
 
 echo "Fetching Fedi data (fedilist)"
-python3 ./fetch-fedilist.py data/fedi/${TIMESTAMP}__fedilist-fromhtml.csv
+python3 ./data-fetchers/fetch-fedilist.py data/fedi/${TIMESTAMP}__fedilist-fromhtml.csv
 
 echo "Fetching Git data"
-python3 ./fetch-sh.py data/git/${TIMESTAMP}__sh-fromhtml.csv
+python3 ./data-fetchers/fetch-sh.py data/git/${TIMESTAMP}__sh-fromhtml.csv
