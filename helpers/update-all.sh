d04f63e199ceba9523cb702ef31c115a89b87e4d
Robert Ricci <ricci@cs.utah.edu>
2025-11-09 15:50:46 -0700

    Make it explicit which fedi source we are fetching

--- helpers/update-all.sh
+++ helpers/update-all.sh
@@ -7,7 +7,7 @@ echo "Running at $TIMESTAMP"
 echo "Fetching BlueSky data"
 python3 ./fetch-bsky.py data/at/${TIMESTAMP}__atproto-bsky-relay.csv
 
-echo "Fetching Fedi data"
+echo "Fetching Fedi data (fedilist)"
 python3 ./fetch-fedilist.py data/fedi/${TIMESTAMP}__fedilist-fromhtml.csv
 
 echo "Fetching Git data"
