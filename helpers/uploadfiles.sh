c250d47af4f0b2255bdad805f8a00808ee55070a
Robert Ricci <ricci@cs.utah.edu>
2026-01-13 13:59:17 -0700

    Switch to uploading with rsync, and upload some of the CSVs
    Trying to copy more of the raw data over was too slow, so I'll just
    handle that through NFS mounts

--- helpers/uploadfiles.sh
+++ helpers/uploadfiles.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-cd /home/ricci/are-we-decentralized-yet/www && tar cvf - * | kubectl exec -i nginx-distributed-writer-0 -- tar xf - -C /usr/share/nginx/html
+AWDY=/home/ricci/are-we-decentralized-yet/
+SSH="ssh -i \"$AWDY/private/id_k8scopy\""
+RDIR=/usr/share/nginx/html
+
+rsync -avz -e "$SSH" $AWDY/www/ rsync@10.1.0.9:$RDIR
+rsync -avz -e "$SSH" $AWDY/data/at-mau rsync@10.1.0.9:$RDIR/data
+rsync -avz -e "$SSH" $AWDY/data/fedi-mau rsync@10.1.0.9:$RDIR/data
+#rsync -avz -e "$SSH" $AWDY/data/bluesky-relay.json rsync@10.1.0.9:$RDIR/data/at-mau-snapshots/
+#rsync -avz -e "$SSH" $AWDY/data/blacksky-relay.json rsync@10.1.0.9:$RDIR/data/at-mau-snapshots/
+#rsync -avz -e "$SSH" $AWDY/data/nodelists rsync@10.1.0.9:$RDIR/data/
+#rsync -avz -e "$SSH" $AWDY/data/nodeinfo rsync@10.1.0.9:$RDIR/data/
+
