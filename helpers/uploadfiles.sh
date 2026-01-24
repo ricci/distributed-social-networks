b874a51937e94b920bfac37b73246d5778fe4bda
Robert Ricci <ricci@cs.utah.edu>
2026-01-23 23:38:16 -0700

    Upload historical data files for bluesky verifiers and fedi software

--- helpers/uploadfiles.sh
+++ helpers/uploadfiles.sh
@@ -7,6 +7,9 @@ RDIR=/usr/share/nginx/html
 rsync -avz -e "$SSH" $AWDY/www/ rsync@10.1.0.9:$RDIR
 rsync -avz -e "$SSH" $AWDY/data/at-mau rsync@10.1.0.9:$RDIR/data
 rsync -avz -e "$SSH" $AWDY/data/fedi-mau rsync@10.1.0.9:$RDIR/data
+rsync -avz -e "$SSH" $AWDY/data/fedi-mau rsync@10.1.0.9:$RDIR/data
+rsync -avz -e "$SSH" $AWDY/data/fedi-software rsync@10.1.0.9:$RDIR/data
+rsync -avz -e "$SSH" $AWDY/data/bsky-verifiers rsync@10.1.0.9:$RDIR/data
 #rsync -avz -e "$SSH" $AWDY/data/bluesky-relay.json rsync@10.1.0.9:$RDIR/data/at-mau-snapshots/
 #rsync -avz -e "$SSH" $AWDY/data/blacksky-relay.json rsync@10.1.0.9:$RDIR/data/at-mau-snapshots/
 #rsync -avz -e "$SSH" $AWDY/data/nodelists rsync@10.1.0.9:$RDIR/data/
